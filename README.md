f6f91c361c42da07275c483abed73f12d9e7386c
dookerdo <dookerdo@gmail.com>
2013-09-24 18:22:26 -0500

    Improved README. Added center, tile, max settings. Cleaned up the UI.

--- README.md
+++ README.md
@@ -1,4 +1,29 @@
 fehgui
 ======
 
-A perl/tk frontend to the feh wallpaper utility
+<b>*</b> WHAT IS IT?
+
+A perl/tk frontend to the feh wallpaper utility.
+
+<b>*</b> WHAT CAN IT DO?
+
+It allows the user to quickly switch the wallpaper without resorting to the command line. It does <b>NOT</b> support feh's more complex features as of yet (and may never).
+
+<b>*</b> WHY SHOULD I USE IT?
+
+No reason, really. I made it to quickly choose a wallpaper out of a directory containing close to a thousand different images. It worked well; I decided to put it out there for anyone who may be interested.
+
+<b>*</b> WHAT DEPENDENCIES DOES IT NEED?
+
+Perl and the tk module. This may change if I ever decide to add more features.
+
+<b>*</b> HOW DO I INSTALL?
+
+It's a simple perl script. You can call it directly with the perl interpreter or move it somewhere your $PATH can see and make it executable.
+
+<b>*</b> WILL YOU ADD <i>X</i> FEATURE?
+
+Probably not, but feel free to ask. This is just a side project and as such  it's low on my list of priorities.
+
+
+
--- fehgui.pl
+++ fehgui.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl -sw
 
 use Tk;
-
+use feature qw/switch/;
 #############################
 ### VARIABLES / PREPATORY ###
 my $directory_walls;
@@ -24,36 +24,49 @@ my($frame1,$frame2,$frame3,$frame2_l,$frame2_r,$frame0);
 $frame0 = $mw->Frame(-background => 'black', -padx=>2, -pady=>2)->pack(-fill=>"both");
 $frame1 = $mw->Frame(-background => 'SlateGray', -padx=>2, -pady=>2)->pack(-fill=>"both");
 $frame2 = $mw->Frame(-background => 'SlateGray', -padx=>2, -pady=>2)->pack(-fill=>"both",-expand=>1);
-$frame3 = $mw->Frame(-background => 'SlateGray', -padx=>2, -pady=>2)->pack(-fill=>'both');
 $frame2_l = $frame2->Frame(-background => 'SlateGray');
 $frame2_r = $frame2->Frame(-background => 'red', -padx=>2, -pady=>2);
 $frame2_l->pack(-expand=>1,-anchor=>'w',-side=>'left', -fill=>'both');
 $frame2_r->pack(-expand=>1,-side=>'right', -fill=>'both');
+$frame3 = $mw->Frame(-background => 'SlateGray', -padx=>2, -pady=>2)->pack(-fill=>'both');
 
 ### WIDGET CREATE ###
-my ($brand, $bselect, $bexit, $lbfiles, $rscale, $rfill, $layout);
+my ($brand, $bselect, $bexit, $lbfiles, $rcenter, $rmax, $rtile, $rscale, $rfill, $layout);
 
+# TOP
 $dirx = $frame0->Label(-text=>"$directory_walls",-background=>'white');
 $dirx->pack(-expand=>1,-side=>"left");
 $bdir = $frame0->Button(-text=>'Change Dir',-command =>\&set_dir);
 $bdir->pack(-side=>"left");
 
+# MID
 $brand = $frame1->Button(-text=>'Random',-command =>\&random_wall);
-$rscale = $frame2_l->Radiobutton(-text=>"Scaled",-value=>"scaled",-variable=>\$layout);
+
+$rscale = $frame2_l->Radiobutton(-text=>"Scaled",-highlightbackground=>'SlateGray',-background=>'SlateGray',-value=>"scaled",-variable=>\$layout);
 $rscale->select;
-$rfill = $frame2_l->Radiobutton(-text=>"Fill",-value=>"fill",-variable=>\$layout,-anchor=>'w');
+$rfill = $frame2_l->Radiobutton(-text=>"Fill",-highlightbackground=>'SlateGray',-background=>'SlateGray',-value=>"fill",-variable=>\$layout,-anchor=>'w');
+$rcenter = $frame2_l->Radiobutton(-text=>"Center",-highlightbackground=>'SlateGray',-background=>'SlateGray',-value=>"center",-variable=>\$layout,-anchor=>'w');
+$rmax = $frame2_l->Radiobutton(-text=>"Max",-value=>"max",-background=>'SlateGray',-highlightbackground=>'SlateGray',-variable=>\$layout,-anchor=>'w');
+$rtile = $frame2_l->Radiobutton(-text=>"Tile",-background=>'SlateGray',-highlightbackground=>'SlateGray',-value=>"tile",-variable=>\$layout,-anchor=>'w');
+
 $lbfiles = $frame2_r->Scrolled("Listbox", -scrollbars=>"se", -exportselection=>0,-selectmode=>"single");
+
+# Bottom
 $bselect = $frame3->Button(-text=>'Select',-command =>\&select);
 $bexit = $frame3->Button(-text=>'Close',-command => sub{exit});
 
 
 ### WIDGET PACK/GEO ###
 
-$brand->pack;
+$brand->pack(-expand=>1,-fill=>'x');
 #$rscale->pack(-side=>'left');
 #$rfill->pack(-side=>'left');
 $rscale->grid(-row=>0,-column=>0,-sticky=>'w');
 $rfill->grid(-row=>1,-column=>0,-sticky=>'w');
+$rcenter->grid(-row=>2,-column=>0,-sticky=>'w');
+$rmax->grid(-row=>3,-column=>0,-sticky=>'w');
+$rtile->grid(-row=>4,-column=>0,-sticky=>'w');
+
 $bselect->pack(-side=>'left',-fill=>'both',-expand=>1);
 $bexit->pack(-side=>'right',-fill=>'both');
 $lbfiles->pack(-expand=>1,-fill=>'both');
@@ -86,12 +99,17 @@ sub select{
 # SETS THE WALLPAPER
 sub set_wall{
 	my ($opt,$file);
-	$file = "/home/koni/wallpaper/" . shift;
-	if($layout eq 'scaled'){
-		$opt = "--bg-scale"
-	}elsif($layout eq "fill"){
-		$opt = "--bg-fill"
+	$file = "$directory_walls/" . shift;
+	
+	given($layout){
+		when('scaled'){ $opt='--bg-scale';}
+		when('fill'){ $opt='--bg-fill';}
+		when('max'){ $opt='--bg-max';}
+		when('tile'){ $opt='--bg-tile';}
+		when('center'){ $opt='--bg-center';}
 	}
+
+	# Call feh
 	system("feh $opt '" . $file . "'");
 }
 
